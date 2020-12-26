b2d5d1e0d4f98e50d83e5d359e18e9fbc223f559
tomasz.kowalczewski <tomasz.kowalczewski@gmail.com>
2020-12-26 18:00:44 +0100

    Re #9: Fixed bugs and added test.

--- tjahzi-core/src/main/java/pl/tkowalcz/tjahzi/LogBufferSerializer.java
+++ tjahzi-core/src/main/java/pl/tkowalcz/tjahzi/LogBufferSerializer.java
@@ -13,6 +13,19 @@ public class LogBufferSerializer {
         this.buffer = buffer;
     }
 
+    public int calculateRequiredSizeAscii(
+            Map<String, String> labels,
+            String logLevelLabel,
+            String logLevel,
+            ByteBuffer line
+    ) {
+        int logLineSize = Integer.BYTES + line.remaining();
+        int logLevelSize = calculateOptionalLabelSizeAscii(logLevelLabel, logLevel);
+        int headerAndLabelsSize = calculateRequiredSizeAscii(labels);
+
+        return headerAndLabelsSize + logLineSize + logLevelSize;
+    }
+
     public void writeTo(
             int cursor,
             long timestamp,
@@ -21,14 +34,52 @@ public class LogBufferSerializer {
             String logLevel,
             ByteBuffer line
     ) {
-        cursor = writeHeader(cursor, timestamp, labels);
+        cursor = writeHeader(
+                cursor,
+                timestamp,
+                labels,
+                logLevelLabel != null
+        );
+
         cursor = writeLabels(cursor, labels);
-        cursor = writeLabel(
+        cursor = writeOptionalLabel(
                 cursor,
                 logLevelLabel,
                 logLevel
         );
+
         writeLogLine(cursor, line);
+    }
+
+    private int writeHeader(
+            int cursor,
+            long timestamp,
+            Map<String, String> labels,
+            boolean hasLogLevelLabel
+    ) {
+        buffer.putLong(cursor, timestamp);
+        cursor += Long.BYTES;
+
+        int labelsCount = labels.size();
+        if (hasLogLevelLabel) {
+            labelsCount++;
+        }
+
+        buffer.putInt(cursor, labelsCount);
+        cursor += Integer.BYTES;
+        return cursor;
+    }
+
+    private int writeLabels(int cursor, Map<String, String> labels) {
+        for (Map.Entry<String, String> entry : labels.entrySet()) {
+            cursor = writeLabel(
+                    cursor,
+                    entry.getKey(),
+                    entry.getValue()
+            );
+        }
+
+        return cursor;
     }
 
     private void writeLogLine(int cursor, ByteBuffer line) {
@@ -42,27 +93,7 @@ public class LogBufferSerializer {
         );
     }
 
-    private int writeLabels(int cursor, Map<String, String> labels) {
-        for (Map.Entry<String, String> entry : labels.entrySet()) {
-            cursor = writeLabel(
-                    cursor,
-                    entry.getKey(),
-                    entry.getValue()
-            );
-        }
-        return cursor;
-    }
-
-    private int writeHeader(int cursor, long timestamp, Map<String, String> labels) {
-        buffer.putLong(cursor, timestamp);
-        cursor += Long.BYTES;
-
-        buffer.putInt(cursor, labels.size() + 1);
-        cursor += Integer.BYTES;
-        return cursor;
-    }
-
-    public int writeLabel(
+    private int writeLabel(
             int cursor,
             String key,
             String value
@@ -73,32 +104,42 @@ public class LogBufferSerializer {
         return cursor;
     }
 
+    private int writeOptionalLabel(
+            int cursor,
+            String key,
+            String value
+    ) {
+        if (key != null) {
+            cursor += buffer.putStringAscii(cursor, key);
+            cursor += buffer.putStringAscii(cursor, value);
+        }
+
+        return cursor;
+    }
+
     private int calculateRequiredSizeAscii(Map<String, String> labels) {
         int timestampSize = Long.BYTES;
         int labelsCountSize = Integer.BYTES;
         int sum = timestampSize + labelsCountSize;
 
         for (Map.Entry<String, String> entry : labels.entrySet()) {
-            sum += calculateLabeSizeAscii(entry.getKey(), entry.getValue());
+            sum += calculateLabelSizeAscii(entry.getKey(), entry.getValue());
         }
 
         return sum;
     }
 
-    private int calculateLabeSizeAscii(String key, String value) {
+    private int calculateOptionalLabelSizeAscii(String key, String value) {
+        if (key == null) {
+            return 0;
+        }
+
         return Integer.BYTES + key.length()
                 + Integer.BYTES + value.length();
     }
 
-    public int calculateRequiredSizeAscii(
-            Map<String, String> labels,
-            String logLevelLabel,
-            String logLevel,
-            ByteBuffer line
-    ) {
-        int lineSize = Integer.BYTES + line.remaining();
-        int logLevelSize = calculateLabeSizeAscii(logLevelLabel, logLevel);
-
-        return calculateRequiredSizeAscii(labels) + lineSize + logLevelSize;
+    private int calculateLabelSizeAscii(String key, String value) {
+        return Integer.BYTES + key.length()
+                + Integer.BYTES + value.length();
     }
 }
