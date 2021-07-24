2e9653a156f4172dfb2fc29f57d4829c3d6319bf
tomasz.kowalczewski <tomasz.kowalczewski@gmail.com>
2021-07-24 10:54:28 +0200

    Removed spurious newline.

--- core/src/main/java/pl/tkowalcz/tjahzi/LabelSerializers.java
+++ core/src/main/java/pl/tkowalcz/tjahzi/LabelSerializers.java
@@ -4,7 +4,6 @@ public class LabelSerializers {
 
     private static final ThreadLocal<LabelSerializer> THREAD_LOCAL = ThreadLocal.withInitial(LabelSerializer::new);
 
-
     public static LabelSerializer threadLocal() {
         LabelSerializer result = THREAD_LOCAL.get();
         result.clear();
