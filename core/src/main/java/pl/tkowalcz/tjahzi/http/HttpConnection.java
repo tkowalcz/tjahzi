79148ac1e3c915f86c0c1da8020135a15f683591
tomasz.kowalczewski <tomasz.kowalczewski@gmail.com>
2021-10-23 17:15:50 +0200

    Re #77: Debugging

--- core/src/main/java/pl/tkowalcz/tjahzi/http/HttpConnection.java
+++ core/src/main/java/pl/tkowalcz/tjahzi/http/HttpConnection.java
@@ -28,6 +28,7 @@ public class HttpConnection implements Closeable {
         EventLoopGroupRetry retry = new EventLoopGroupRetry(
                 group,
                 __ -> {
+                    System.out.println("INCREMENT...");
                     monitoringModule.incrementHttpConnectAttempts();
                     recreateConnection(__);
                 }
