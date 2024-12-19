ca012596fc8c9da8eb5d7e3f89b477dbabe2fde8
Filipe Roque <froque@premium-minds.com>
2024-12-19 23:33:10 +0000

    Change name for ProtobufDeserializer
    maven-surefire-plugin only runs tests starting or ending with Test.
    https://maven.apache.org/surefire/maven-surefire-plugin/examples/inclusion-exclusion.html

--- core/src/test/java/pl/tkowalcz/thjazi/protobuf/ProtobufDeserializerTest.java
+++ core/src/test/java/pl/tkowalcz/thjazi/protobuf/ProtobufDeserializerTest.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.Map;
 
-public class ProtobufDeserializer {
+public class ProtobufDeserializerTest {
 
     @Test
     void shouldDeserialize() throws IOException {
