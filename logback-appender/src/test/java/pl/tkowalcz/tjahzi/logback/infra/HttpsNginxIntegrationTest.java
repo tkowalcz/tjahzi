c05cf15053d0bfda48996a5ff957ba3a6c3aa61d
Tomasz Kowalczewski <tkowalczewski@voltdb.com>
2025-09-20 14:32:51 +0200

    155: Support for a trust store and various certificate types.

---
+++ logback-appender/src/test/java/pl/tkowalcz/tjahzi/logback/infra/HttpsNginxIntegrationTest.java
@@ -0,0 +1,44 @@
+package pl.tkowalcz.tjahzi.logback.infra;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.NginxContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
+
+public class HttpsNginxIntegrationTest extends IntegrationTest {
+
+    @Container
+    @SuppressWarnings("resource")
+    public NginxContainer<?> nginx = new NginxContainer<>("nginx:1.25")
+            .withNetwork(network)
+            .withClasspathResourceMapping("loki.reverse.nginx.conf",
+                    "/etc/nginx/conf.d/loki.reverse.conf",
+                    BindMode.READ_ONLY
+            )
+            .withClasspathResourceMapping("nginx/nginx-selfsigned.crt",
+                    "/etc/nginx/nginx-selfsigned.crt",
+                    BindMode.READ_ONLY
+            )
+            .withClasspathResourceMapping("nginx/nginx-selfsigned.key",
+                    "/etc/nginx/nginx-selfsigned.key",
+                    BindMode.READ_ONLY
+            )
+            .withClasspathResourceMapping("nginx/passwords",
+                    "/etc/nginx/passwords",
+                    BindMode.READ_ONLY
+            )
+            .waitingFor(Wait
+                    .forHttp("/")
+                    .forStatusCode(200)
+                    .forStatusCode(400)
+            )
+            .withExposedPorts(81);
+
+    @Override
+    @BeforeEach
+    public void setUp() throws Exception {
+        System.setProperty("loki.host", nginx.getHost());
+        System.setProperty("loki.port", nginx.getFirstMappedPort().toString());
+    }
+}
