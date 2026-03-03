5535c9faf70d4ee97063a4f057f8439f4f0b9745
Tomasz Kowalczewski <tkowalczewski@voltdb.com>
2026-03-03 09:29:56 +0100

    158: Test fixes.

--- log4j2-appender/src/test/java/pl/tkowalcz/tjahzi/log4j2/LokiAppenderHttpsCustomEndpointTest.java
+++ log4j2-appender/src/test/java/pl/tkowalcz/tjahzi/log4j2/LokiAppenderHttpsCustomEndpointTest.java
@@ -2,15 +2,29 @@ package pl.tkowalcz.tjahzi.log4j2;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.tkowalcz.tjahzi.log4j2.infra.HttpsNginxIntegrationTest;
 import pl.tkowalcz.tjahzi.log4j2.infra.LokiAssert;
+import pl.tkowalcz.tjahzi.log4j2.infra.TruststoreUtil;
+
+import java.io.File;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
 
 class LokiAppenderHttpsCustomEndpointTest extends HttpsNginxIntegrationTest {
+
+    @Override
+    @BeforeEach
+    public void setUp() throws Exception {
+        super.setUp();
+
+        File ts = TruststoreUtil.createPkcs12TruststoreFromPem("nginx/nginx-selfsigned.crt", "changeit");
+        System.setProperty("javax.net.ssl.trustStore", ts.getAbsolutePath());
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
+    }
 
     @Test
     void shouldSendData() {
